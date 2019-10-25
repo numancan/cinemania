01b9180454cb45171dca78ce10bf53475c8205ba
Numan Can İpekçi <n.canipekci@gmail.com>
2019-10-25 12:46:11 +0300

    Updates

---
+++ precache-manifest.9770dae3514c77855b0a50018b968d41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9b449ecb673e99be367fb75c874927",
+    "url": "/cinemania/index.html"
+  },
+  {
+    "revision": "61a7b02844d6177f2ab1",
+    "url": "/cinemania/static/css/3.f05ac564.chunk.css"
+  },
+  {
+    "revision": "d82570101c82e3abf68b",
+    "url": "/cinemania/static/css/4.0fbce8ee.chunk.css"
+  },
+  {
+    "revision": "fc4a5c56dc7cd0cdf3ab",
+    "url": "/cinemania/static/css/main.18c839d9.chunk.css"
+  },
+  {
+    "revision": "a5b16ad65e0caa87be2a",
+    "url": "/cinemania/static/js/2.a36376ed.chunk.js"
+  },
+  {
+    "revision": "61a7b02844d6177f2ab1",
+    "url": "/cinemania/static/js/3.29b4a2bd.chunk.js"
+  },
+  {
+    "revision": "d82570101c82e3abf68b",
+    "url": "/cinemania/static/js/4.e9aa6001.chunk.js"
+  },
+  {
+    "revision": "fc4a5c56dc7cd0cdf3ab",
+    "url": "/cinemania/static/js/main.148a91b7.chunk.js"
+  },
+  {
+    "revision": "73d49a3e9a6459145b7c",
+    "url": "/cinemania/static/js/runtime-main.3fb529e1.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cinemania/precache-manifest.9770dae3514c77855b0a50018b968d41.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cinemania/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
