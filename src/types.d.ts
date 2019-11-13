b2c4e653eb4e7e6673e556582c6e28baa928c716
Numan Can İpekçi <n.canipekci@gmail.com>
2019-11-13 20:45:05 +0300

    Added new types

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,10 +5,20 @@ interface Movie {
   backdrop_path?: string;
 }
 
+interface Cast {
+  id: string;
+  name: string;
+  profile_path: string;
+  character: string;
+}
+
 interface MovieDetails extends Movie {
   overview: string;
-  credits: { cast: Array<{ profile_path: string }> };
+  credits: { cast: Array<Cast> };
   videos: { results: Array<{ key: string; name: string }> };
+  genres: Array<{ name: string }>;
+  runtime: string;
+  vote_average?: string;
   hall_id?: number;
 }
 
